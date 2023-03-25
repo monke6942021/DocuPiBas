5b06bfebfb2606645790e0cdcf7c795219d90166
monke6942021 <mbhatta2@ucsc.edu>
2023-03-24 17:09:48 -0700

    I should also leave a reference to the updated average query model.

--- timed_gunreg_test.py
+++ timed_gunreg_test.py
@@ -1,14 +1,14 @@
 import os
 
 i = 10
+
 while i <= 100000:
     f = open("query_count.txt", "w")
     f.write(f"{i}")
     f.close()
-    print(f"Now testing with database of size {i}")
-    for j in range(11):
-        print(f"Trial {j}")
-        os.system("python3 create_gunreg_input.py < query_count.txt")
-        os.system("python3 test/test_sse_schemes/timed_gunreg_model.py < input.txt")
-    print()
+    times = []
+    print(f"Handling Database of size {i}")
+    os.system("python3 create_gunreg_input.py < query_count.txt")
+    os.system("python3 test/test_sse_schemes/timed_gunreg_model.py < input.txt > output.txt")
+    os.system("python3 test/test_sse_schemes/timed_gunreg_model.py < input.txt")
     i *= 10
